30e9763eb6be7dc0691c430e54428f0853471a59
ycc <523810076@qq.com>
2016-05-18 17:57:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dicky/selfpicker/fortext/MixPicker.java
@@ -0,0 +1,14 @@
+package com.example.dicky.selfpicker.fortext;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.example.dicky.selfpicker.R;
+
+public class MixPicker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mix_picker);
+    }
+}
---
+++ selecttime/src/main/java/com/example/selecttime/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.selecttime;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends Activity {
+	WheelMain wheelMain;
+	EditText txttime;
+	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        txttime = (EditText)findViewById(R.id.txttime);
+	  	Calendar calendar = Calendar.getInstance();
+	  	txttime.setText(calendar.get(Calendar.YEAR) + "-" +
+	  				    (calendar.get(Calendar.MONTH) + 1 )+ "-" +
+	  				    (calendar.get(Calendar.DAY_OF_MONTH) + "-")+
+	  				  (calendar.get(Calendar.HOUR_OF_DAY) + "-")+
+	  				    calendar.get(Calendar.MINUTE) + "");
+        Button btnselecttime = (Button)findViewById(R.id.button1);
+		btnselecttime.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				LayoutInflater inflater=LayoutInflater.from(MainActivity.this);
+				final View timepickerview=inflater.inflate(R.layout.timepicker, null);
+				ScreenInfo screenInfo = new ScreenInfo(MainActivity.this);
+				wheelMain = new WheelMain(timepickerview);
+				wheelMain.screenheight = screenInfo.getHeight();
+				String time = txttime.getText().toString();
+				Calendar calendar = Calendar.getInstance();
+				if(JudgeDate.isDate(time, "yyyy-MM-dd hh:mm")){
+					try {
+						calendar.setTime(dateFormat.parse(time));
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				int year = calendar.get(Calendar.YEAR);
+				int month = calendar.get(Calendar.MONTH);
+				int day = calendar.get(Calendar.DAY_OF_MONTH);
+				int hour=calendar.get(Calendar.HOUR_OF_DAY);
+				int minute=calendar.get((Calendar.MINUTE));
+				wheelMain.initDateTimePicker(year,month,day,hour,minute);
+				new AlertDialog.Builder(MainActivity.this)
+				.setTitle("    ��  		��  		��  		ʱ 	  ��     ")
+				.setView(timepickerview)
+				.setPositiveButton("ȷ��", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						txttime.setText(wheelMain.getTime());
+					}
+				})
+				.setNegativeButton("ȡ��", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+					}
+				})
+				.show();
+			}
+		});
+    }
+}
